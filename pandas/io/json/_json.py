d93d35c0b7ce6d74e375751b97b578c5e02ed454
Кирилл Потапов <kdpotapov02@gmail.com>
2024-06-09 15:09:27 +0000

    ENH: Add the ability to read Django fixtures in JSON format into a DataFrame.

--- pandas/io/json/_json.py
+++ pandas/io/json/_json.py
@@ -1346,6 +1346,12 @@ class FrameParser(Parser):
             )
         elif orient == "table":
             self.obj = parse_table_schema(json, precise_float=self.precise_float)
+        elif orient == "fixtures":
+            decoded = DataFrame(
+                ujson_loads(json, precise_float=self.precise_float), dtype=None
+            )
+            data = DataFrame(decoded["fields"].to_dict()).T
+            self.obj = concat([decoded["model"], decoded["pk"], data], axis=1)
         else:
             self.obj = DataFrame(
                 ujson_loads(json, precise_float=self.precise_float), dtype=None
